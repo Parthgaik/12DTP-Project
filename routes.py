0804f566675488980a98c9445130214a380e8dc4
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-07-20 14:56:26 +1200

    Added "seats" data into website

--- routes.py
+++ routes.py
@@ -1,53 +1,44 @@
-
 from flask import Flask,render_template
-#Importing sqlite3
+# Importing sqlite3
 import sqlite3
 
 
 app = Flask (__name__)
 
 
-#Home Route
+# Home Route
 @app.route('/')
 def home():
-    return render_template("home.html",title = "Home")
+    return render_template("home.html", title="Home")
 
 
-#Drivers Route
+# Drivers Route
 @app.route('/drivers')
 def drivers():
-    conn =sqlite3.connect("F1.db")
-    cursor =conn.cursor()
+    conn = sqlite3.connect("F1.db")
+    cursor = conn.cursor()
     cursor.execute("SELECT * FROM Drivers")
     drivers = cursor.fetchall()
-    return render_template("drivers.html", title = "Drivers", drivers = drivers)
-#Teams Route
+    return render_template("drivers.html", title="Drivers", drivers=drivers)
+# Teams Route
 @app.route('/teams')
 def teams():
-    conn =sqlite3.connect("F1.db")
-    cursor =conn.cursor()
+    conn = sqlite3.connect("F1.db")
+    cursor = conn.cursor()
     cursor.execute("SELECT * FROM Teams")
     teams = cursor.fetchall()
-    return render_template("teams.html",title = "Teams", teams=teams)
-#Seats Route
+    return render_template("teams.html", title="Teams", teams=teams)
+# Seats Route
+
+
 @app.route('/seats')
 def seats():
-    conn=sqlite3.connect("F1.db")
-    cursor=conn.cursor()
+    conn = sqlite3.connect("F1.db")
+    cursor = conn.cursor()
     cursor.execute("SELECT * FROM Seat")
     seat = cursor.fetchall()
-    return render_template("seats.html",title = "Seats", seat=seat)
-
-
-
-
-
-
-
-
-
-
+    return render_template("seats.html", title="Seats", seat=seat)
 
 
 if __name__ == "__main__":
-    app.run(debug =True)
+    app.run(debug=True)
