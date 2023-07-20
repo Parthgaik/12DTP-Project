a2286e809cf25c977e40719950f4ee1c6b13739e
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-07-20 13:38:21 +1200

    Worked trying to get my seats route and information working

--- routes.py
+++ routes.py
@@ -1,4 +1,6 @@
+
 from flask import Flask,render_template
+#Importing sqlite3
 import sqlite3
 
 
@@ -19,14 +21,22 @@ def drivers():
     cursor.execute("SELECT * FROM Drivers")
     drivers = cursor.fetchall()
     return render_template("drivers.html", title = "Drivers", drivers = drivers)
-
+#Teams Route
 @app.route('/teams')
 def teams():
     conn =sqlite3.connect("F1.db")
     cursor =conn.cursor()
-    cursor.execute("SELECT * FROM TEAMS")
+    cursor.execute("SELECT * FROM Teams")
     teams = cursor.fetchall()
     return render_template("teams.html",title = "Teams", teams=teams)
+#Seats Route
+@app.route('/seats')
+def seats():
+    conn=sqlite3.connect("F1.db")
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM Seat")
+    seat = cursor.fetchall()
+    return render_template("seats.html",title = "Seats", seat=seat)
 
 
 
