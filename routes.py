5a3332545f590647b056a9995636ed6a2844ee15
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-08-17 14:42:28 +1200

    more commenting, and removed the login stuff

--- routes.py
+++ routes.py
@@ -1,6 +1,11 @@
+#importing flask, and render template
 from flask import Flask,render_template
 # Importing sqlite3
 import sqlite3
+
+app = Flask(__name__)
+
+
 
 # Connect database (F1.db), get cursor, execute cursor with statement and id, fetchall() results and return results
 def connect_database_id(statement, id):
@@ -11,7 +16,7 @@ def connect_database_id(statement, id):
     conn.close()
     return results
 
-#
+# Connects the databse and then executes the statement, fetches all the results, closes the connection and then returns the values from the results.
 def connect_database(statement):
     conn = sqlite3.connect("F1.db")
     cursor = conn.cursor()
@@ -20,61 +25,45 @@ def connect_database(statement):
     conn.close()
     return results
 
-
-app = Flask(__name__)
-
-
-# Home Route
+# Home Route, takes the user to the home page
 @app.route('/')
 def home():
     return render_template("home.html", title="Home")
 
 
-# Drivers Route
+# Drivers Route, gets the id, name and image from drivers and delivers it to the all_drivers.html
 @app.route('/all_drivers')
 def all_drivers():
-    conn = sqlite3.connect("F1.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT id, name, Image FROM Drivers")
     drivers = connect_database("SELECT id, name, Image FROM Drivers")
     return render_template("all_drivers.html", title="Drivers", drivers=drivers)
 
 
-# Teams Route
+# Teams Route, gets everything from teams and then delivers it to the teams.html
 @app.route('/teams')
 def teams():
-    conn = sqlite3.connect("F1.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Teams")
-    teams = cursor.fetchall()
+    teams = connect_database("SELECT * FROM Teams")
     return render_template("teams.html", title="Teams", teams=teams)
 
 
-# Seats Route
+# Seats Route, gets all everythig from seats and then delivers it to the seats html
 @app.route('/seats')
 def seats():
-    conn = sqlite3.connect("F1.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Seat")
-    seat = cursor.fetchall()
+    seat = connect_database("SELECT * FROM Seat")
     return render_template("seats.html", title="Seats", seat=seat)
 
-
+# Driver route, gets a specific driver's entry with the given id, then renders driver.html
 @app.route('/drivers/<int:id>')
-def drivers(id):
+def driver(id):
     driver = connect_database_id("SELECT * FROM Drivers WHERE id =?", (id,))
     return render_template("driver.html", title="Driver", driver=driver)
-    
-
+     
+# Teams route, gets a specific team's entry with the given id, then renders teams.html
 @app.route('/teams/<int:id>')
 def team(id):
     teams = connect_database_id("SELECT * FROM Teams WHERE id =?", (id,))
     return render_template("team.html", title="Team", teams=teams)
 
 
-@app.route('/signup')
-def signup():
-    return render_template("Signup.html", title="Sign up")
 
 
 if __name__ == "__main__":
